48eba88a1abd38245f5b1be4349226af54d65d7d
Mathias Magnusson <mathias.magnusson@gmail.com>
2019-01-24 12:25:21 +0100

    Improvements for APEX-extention

--- dockerimages/buildall.sh
+++ dockerimages/buildall.sh
@@ -37,27 +37,37 @@ if [ ! -f "oracle-database-xe-18c-1.0-1.x86_64.rpm" ]; then
   exit
 fi
 
-if [ ! -f "apex_*.zip" ]; then
+if (( 0 == $(ls apex_*.zip 2>/dev/null | wc -w) )); then
   echo 'The Oracle APEX installation zip-file needs to be present and located in the same directory as this script.'
   exit
 fi
 
+echo 'Copying files'
 cp oracle-database-xe-18c-1.0-1.x86_64.rpm $ORA_IMAGES_DIR/docker-images-master/OracleDatabase/SingleInstance/dockerfiles/18.4.0/
 cp apex_*.zip OracleAPEX
 #mv  ords-18*.zip jre-8u*-linux-x64.tar.gz sqlcl-*.zip oracle-database-xe-18c-1.0-1.x86_64.rpm 
 
+echo 'Build oracle/database:18.4.0-xe'
+
 # Build the Oracle XE 18.4.0 image
 cd $ORA_IMAGES_DIR/docker-images-master/OracleDatabase/SingleInstance/dockerfiles
-./buildDockerImage.sh -x -v 18.4.0
+./buildDockerImage.sh -x -v 18.4.0 > run.log
 
 if [ $(docker image ls -q oracle/database:18.4.0-xe | wc -l) == '0' ]; then
     echo 'The build of Oracle XE did not succeed. Exiting.'
     exit
 fi
 
+echo 'Build evilape/database:18.4.0-xe_w_apex'
+
 cd $SCRIPT_DIR/OracleAPEX
 
-docker build -t evilape/database:18.4.0-xe_w_apex -f Dockerfile .
+docker build -t evilape/database:18.4.0-xe_w_apex -f Dockerfile . >> run.log
+
+if [ $(docker image ls -q evilape/database:18.4.0-xe_w_apex | wc -l) == '0' ]; then
+    echo 'The build of Oracle XE did not succeed. Exiting.'
+    exit
+fi
 
 #docker network create --driver bridge oracle_isolated_network
 
